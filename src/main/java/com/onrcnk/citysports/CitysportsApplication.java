39feb14071403aced75f0b4e392d31c4baacfe97
Onrcnk <onrcnk@hotmail.com>
2022-03-20 09:58:41 +0300

    first commit

---
+++ src/main/java/com/onrcnk/citysports/CitysportsApplication.java
@@ -0,0 +1,13 @@
+package com.onrcnk.citysports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitysportsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitysportsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/onrcnk/citysports/CitysportsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onrcnk.citysports;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitysportsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
